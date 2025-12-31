8ff4a124bee948405952756cf02a2188e2fcadeb
akash kumar <akash639673@gmail.com>
2025-12-31 16:41:56 +0530

    update ping route

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ import noteRoutes from "./routes/noteRoutes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/ping', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
 app.use("/api/notes", noteRoutes);
 
 export default app;
